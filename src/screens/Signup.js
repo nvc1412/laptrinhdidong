52461b395646a5abf03eb98e19cbddd9849e18b6
cuong <nvc14122002@gmail.com>
2022-10-04 20:18:04 +0700

    API NodeJs , Bai TH 4/10/2022

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -4,12 +4,14 @@ import MainLogo from "../components/MainLogo";
 import MainInput from "../components/MainInput";
 import React, { useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { response } from "express";
 
 export default function Signup({ navigation }) {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setname] = useState("");
+  const [phone, setphone] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
   const onGoBack = () => {
     navigation.goBack();
   };
@@ -23,63 +25,85 @@ export default function Signup({ navigation }) {
     } else if (password.trim() == "" || !password) {
       alert("Không được để trống mật khẩu !");
     } else {
-      createAccount();
+      //createAccount();
+      dnPost.bind(this);
     }
   };
-  const createAccount = async () => {
-    let userData = await AsyncStorage.getItem("userData");
-    if (userData) {
-      userData = JSON.parse(userData);
-      let arr = [...userData];
-      arr = arr.filter(
-        (value) => value.email.toLocaleLowerCase() == email.toLocaleLowerCase()
-      );
-      if (arr.length > 0) {
-        alert("Email already registered!");
-        return;
-      } else {
-        userData.push({
-          name: name.trim(),
-          phone: phone.trim(),
-          email: email.trim(),
-          password: password.trim(),
-        });
-      }
-    } else {
-      userData = [];
-      userData.push({
+
+  const dnPost = () => {
+    var url = "http://192.168.180.188:3001/data";
+    axios
+      .post(url, {
         name: name.trim(),
         phone: phone.trim(),
         email: email.trim(),
         password: password.trim(),
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    }
-    AsyncStorage.setItem("userData", JSON.stringify(userData));
-    alert("Đăng ký thành công!");
-    navigation.goBack();
+    name = "";
+    phone = "";
+    email = "";
+    password = "";
   };
+
+  // const createAccount = async () => {
+  //   let userData = await AsyncStorage.getItem("userData");
+  //   if (userData) {
+  //     userData = JSON.parse(userData);
+  //     let arr = [...userData];
+  //     arr = arr.filter(
+  //       (value) => value.email.toLocaleLowerCase() == email.toLocaleLowerCase()
+  //     );
+  //     if (arr.length > 0) {
+  //       alert("Email already registered!");
+  //       return;
+  //     } else {
+  //       userData.push({
+  //         name: name.trim(),
+  //         phone: phone.trim(),
+  //         email: email.trim(),
+  //         password: password.trim(),
+  //       });
+  //     }
+  //   } else {
+  //     userData = [];
+  //     userData.push({
+  //       name: name.trim(),
+  //       phone: phone.trim(),
+  //       email: email.trim(),
+  //       password: password.trim(),
+  //     });
+  //   }
+  //   AsyncStorage.setItem("userData", JSON.stringify(userData));
+  //   alert("Đăng ký thành công!");
+  //   navigation.goBack();
+  // };
 
   return (
     <View style={{ marginTop: 70 }}>
       <MainLogo title="Create new account" />
 
-      <MainInput placeholder="Full Name" value={name} onchangeText={setName} />
+      <MainInput placeholder="Full Name" value={name} onChangeText={setname} />
       <MainInput
         placeholder="Phone Number"
         value={phone}
-        onchangeText={setPhone}
+        onChangeText={setphone}
       />
       <MainInput
         placeholder="E-mail Adress"
         value={email}
-        onchangeText={setEmail}
+        onChangeText={setemail}
       />
       <MainInput
         placeholder="Password"
         value={password}
-        onchangeText={setPassword}
+        onChangeText={setpassword}
       />
-
       <View
         style={{
           width: "100%",
